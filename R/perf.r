97651ca0ff72fe44fa29aca3df8e0490ed6800f3
Jesus Escalante <jesus.escalante@heracleaweb.com>
2019-06-22 16:45:35 -0500

    fix prediction variable name

--- R/perf.r
+++ R/perf.r
@@ -36,7 +36,7 @@ perf <- function(nn) {
         model0$sigma <- nn$sigma
         X <- ts$one[-nn$category.column]
         prediction <- guess(model0, as.matrix(X))
-        category <- ifelse(!is.na(pred), pred$category, pred)
+        category <- ifelse(!is.na(prediction), prediction$category, prediction)
         if(!is.na(category) && category == ts$one[nn$category.column]) success <- success + 1
         guessed <- c(guessed, category)
     }
